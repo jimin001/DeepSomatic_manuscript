b927c268d76b18f1fbd18686334a8dc81ac5c3bb
Jimin Park <jpark621@phoenix-10.prism>
2025-04-04 00:32:27 -0700

    upload

---
+++ deepsomatic_benchmarks/generate_benchmarks_bed.sh
@@ -0,0 +1,110 @@
+while getopts b:m:t:s:p:o:d:a:b:x:y: flag
+do
+    case "${flag}" in
+        b) bed_file=${OPTARG};;
+        m) merged_vcf=${OPTARG};;
+        t) truth_vcf=${OPTARG};;
+        s) sample=${OPTARG};;
+        p) output_prefix=${OPTARG};;
+        o) output_directory=${OPTARG};;
+		d) date=${OPTARG};;
+        a) sv_bed1=${OPTARG};;
+        b) sv_bed2=${OPTARG};;
+        x) other_regions=${OPTARG};;
+        y) name_other_regions=${OPTARG};;
+
+    esac
+done
+
+echo "bed_file: $bed_file";
+echo "merged_vcf: $merged_vcf";
+echo "truth_vcf: $truth_vcf";
+echo "sample: $sample";
+echo "output_prefix: $output_prefix";
+echo "output_directory: $output_directory";
+echo "date: $date";
+
+# optional parameters
+if [[ "$sv_bed1" != "" ]] ; then
+echo "sv_bed1: $sv_bed1"; fi
+
+if [[ "$sv_bed2" != "" ]] ; then
+echo "sv_bed2: $sv_bed2"; fi
+
+if [[ "$other_regions" != "" ]] ; then
+echo "other_regions: $other_regions"; fi
+
+if [[ "$name_other_region" != "" ]] ; then
+echo "name_other_regions: $name_other_regions"; fi
+
+
+set -o pipefail
+set -e
+set -u
+
+mkdir -p ${output_directory}
+
+# 1. subtract "confusing regions" and generate "high conf regions bed"
+/private/groups/patenlab/jimin/scripts/deepsomatic/subtract_confusing_regions.sh \
+-c ${bed_file} \
+-m ${merged_vcf} \
+-t ${truth_vcf} \
+-s ${sample} \
+-p ${output_prefix} \
+-o ${output_directory} \
+-d ${date}
+
+# output from subtract_confusing_regions.sh
+HIGH_CONF_BED=${output_directory}/${sample}/${date}_${sample}_${output_prefix}_highconf.bed
+
+# 2. subtract SV regions and SD regions
+
+SD_BED=/private/groups/patenlab/jimin/data/BED/GIAB/GRCh38_segdups.bed
+
+if [[ -z "$sv_bed1" ]]
+then
+    bedtools subtract -a ${HIGH_CONF} -b ${SD_BED} > ${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSDs.bed
+    CURR_BED=${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSDs.bed
+elif [[ -z "$sv_bed2" ]]
+then
+    bedtools subtract -a ${HIGH_CONF} -b ${sv_bed1} | bedtools subtract -a stdin -b ${SD_BED} > ${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSVs_SDs.bed
+    CURR_BED=${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSVs_SDs.bed
+else
+    bedtools subtract -a ${HIGH_CONF} -b ${sv_bed1} | bedtools subtract -a stdin -b ${sv_bed2} | bedtools subtract -a stdin -b ${SD_BED} > ${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSVs_SDs.bed
+    CURR_BED=${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSVs_SDs.bed
+fi
+
+# 3. subtract any other regions
+
+if [[ "$other_regions" != "" ]]
+then
+    if [[ "${CURR_BED}" == ${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSVs_SDs.bed ]]
+    then
+        bedtools subtract -a ${CURR_BED} -b ${other_regions} > ${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSVs_SDs_${name_other_regions}.bed
+
+        # update CURR_BED
+        CURR_BED=${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSVs_SDs_${name_other_regions}.bed
+    elif [[ "${CURR_BED}" == ${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSDs.bed ]]
+    then
+        bedtools subtract -a ${CURR_BED} -b ${other_regions} > ${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSDs_${name_other_regions}.bed
+
+        # update CURR_BED
+        CURR_BED=${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSDs_${name_other_regions}.bed
+fi
+
+
+# 4. generate autosome only verions (chr1-22)
+# (if statements just for correct file name)
+if [[ "${CURR_BED}" == ${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSVs_SDs.bed ]]
+then
+    grep -v "chrX" ${CURR_BED} | grep -v "chrY" > ${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSVs_SDs_chr1_22.bed
+elif [[ "${CURR_BED}" == ${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSDs.bed ]]
+then
+    grep -v "chrX" ${CURR_BED} | grep -v "chrY" > ${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSDs_chr1_22.bed
+elif [[ "${CURR_BED}" == ${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSVs_SDs_${name_other_regions}.bed ]]
+then 
+    grep -v "chrX" ${CURR_BED} | grep -v "chrY" > ${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSVs_SDs_${name_other_regions}_chr1_22.bed
+elif
+    [[ "${CURR_BED}" == ${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSDs_${name_other_regions}.bed ]]
+then
+    grep -v "chrX" ${CURR_BED} | grep -v "chrY" > ${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSDs_${name_other_regions}_chr1_22.bed
---
+++ deepsomatic_benchmarks/generate_benchmarks_vcf.sh
@@ -0,0 +1,112 @@
+while getopts i:h:o:s:d:c:v:f: flag
+do
+    case "${flag}" in
+        i) illumina_vcf=${OPTARG};;
+        h) hifi_vcf=${OPTARG};;
+        o) ont_vcf=${OPTARG};;
+        s) sample=${OPTARG};;
+        d) output_directory=${OPTARG};;
+		c) variant_caller=${OPTARG};;
+        v) version=${OPTARG};;
+		f) filter=${OPTARG};;
+    esac
+done
+
+echo "illumina_vcf: $illumina_vcf";
+echo "hifi_vcf: $hifi_vcf";
+echo "ont_vcf: $ont_vcf";
+echo "sample: $sample";
+echo "output_directory: $output_directory";
+echo "variant_caller: $variant_caller";
+echo "variant caller options: deepsomatic or clairs"
+echo "variant caller version: $version";
+echo "filter: $filter";
+echo "filter options: 'filter4', 'orthogonal technology'"
+
+
+set -o pipefail
+set -e
+set -u
+
+# input files should be filtered for somatic-only
+
+if [[ $filter -eq "filter4" ]]
+then
+	#########################################
+	# generate filter4 benchmark 
+	#########################################
+
+	## merge
+	bcftools merge --force-samples --threads 16 ${illumina_vcf} ${hifi_vcf} ${ont_vcf} | bgzip > ${output_directory}/${sample}_${variant_caller}_${version}_Illumina_PacBio_ONT_somaticOnly_merged.vcf.gz
+	bcftools index -t ${output_directory}/${sample}_${variant_caller}_${version}_Illumina_PacBio_ONT_somaticOnly_merged.vcf.gz
+
+
+	## filter4 on somatic only merged
+
+	TOOL=/private/groups/patenlab/jimin/scripts/deepsomatic/vcf_intersection_complex_v2.py
+
+	MERGE_VCF=${output_directory}/${sample}_${variant_caller}_${version}_Illumina_PacBio_ONT_somaticOnly_merged.vcf.gz
+	OUTPUT=${output_directory}/${sample}_${variant_caller}_${version}_Illumina_PacBio_ONT_somaticOnly_filter4.vcf.gz
+
+	python3 ${TOOL} -v ${MERGE_VCF} -i ${MERGE_VCF}.tbi -f 'filter4' -o ${OUTPUT}
+	bcftools index -t ${OUTPUT}
+elif [[ $filter -eq "orthogonal technology" ]]
+then
+	################################################
+	# generate orthogonal technology benchmark 
+	################################################
+
+	#################################################### MERGE ####################################################
+
+	# WGS + PACBIO ###################
+	bcftools merge --force-samples --threads 4 ${illumina_vcf} ${hifi_vcf} | bgzip > ${output_directory}/${sample}_${variant_caller}_${version}_Illumina_PacBio_somaticOnly_merged.vcf.gz
+	bcftools index -t ${output_directory}/${sample}_${variant_caller}_${version}_Illumina_PacBio_somaticOnly_merged.vcf.gz
+
+	# PACBIO + ONT ###################
+	bcftools merge --force-samples --threads 4 ${hifi_vcf} ${ont_vcf} | bgzip > ${output_directory}/${sample}_${variant_caller}_${version}_PacBio_ONT_somaticOnly_merged.vcf.gz
+	bcftools index -t ${output_directory}/${sample}_${variant_caller}_${version}_PacBio_ONT_somaticOnly_merged.vcf.gz
+
+	# WGS + ONT ###################
+	bcftools merge --force-samples --threads 4 ${illumina_vcf} ${ont_vcf} | bgzip > ${output_directory}/${sample}_${variant_caller}_${version}_Illumina_ONT_somaticOnly_merged.vcf.gz
+	bcftools index -t ${output_directory}/${sample}_${variant_caller}_${version}_Illumina_ONT_somaticOnly_merged.vcf.gz
+
+	#################################################### INTERSECTION ####################################################
+
+	if [[ $variant_caller -eq "deepsomatic" ]]
+	then
+		FILTER='two_tech'
+	elif [[ $variant_caller -eq "clairs" ]]
+	then
+		FILTER='two_tech_clairs'
+	fi
+
+	# WGS + PACBIO ###################
+	VCF=${output_directory}/${sample}_${variant_caller}_${version}_Illumina_PacBio_somaticOnly_merged.vcf.gz
+
+	python3 /private/groups/patenlab/jimin/scripts/deepsomatic/working/vcf_comprehensive_update.py --vcf ${VCF} --vcfindex ${VCF}.tbi \
+	--filter ${FILTER} --outfile ${output_directory}/${sample}_${variant_caller}_${version}_Illumina_PacBio_somaticOnly_intersection.vcf
+
+	bgzip ${output_directory}/${sample}_${variant_caller}_${version}_Illumina_PacBio_somaticOnly_intersection.vcf
+	bcftools index -t ${output_directory}/${sample}_${variant_caller}_${version}_Illumina_PacBio_somaticOnly_intersection.vcf.gz
+
+
+	# PACBIO + ONT ###################
+	VCF=${output_directory}/${sample}_${variant_caller}_${version}_PacBio_ONT_somaticOnly_merged.vcf.gz
+
+	python3 /private/groups/patenlab/jimin/scripts/deepsomatic/working/vcf_comprehensive_update.py --vcf ${VCF} --vcfindex ${VCF}.tbi \
+	--filter ${FILTER} --outfile ${output_directory}/${sample}_${variant_caller}_${version}_PacBio_ONT_somaticOnly_intersection.vcf
+
+	bgzip ${output_directory}/${sample}_${variant_caller}_${version}_PacBio_ONT_somaticOnly_intersection.vcf
+	bcftools index -t ${output_directory}/${sample}_${variant_caller}_${version}_PacBio_ONT_somaticOnly_intersection.vcf.gz
+
+
+	# WGS + ONT ###################
+	VCF=${output_directory}/${sample}_${variant_caller}_${version}_Illumina_ONT_somaticOnly_merged.vcf.gz
+
+	python3 /private/groups/patenlab/jimin/scripts/deepsomatic/working/vcf_comprehensive_update.py --vcf ${VCF} --vcfindex ${VCF}.tbi \
+	--filter ${FILTER} --outfile ${output_directory}/${sample}_${variant_caller}_${version}_Illumina_ONT_somaticOnly_intersection.vcf
+
+	bgzip ${output_directory}/${sample}_${variant_caller}_${version}_Illumina_ONT_somaticOnly_intersection.vcf
+	bcftools index -t ${output_directory}/${sample}_${variant_caller}_${version}_Illumina_ONT_somaticOnly_intersection.vcf.gz
+fi
+
