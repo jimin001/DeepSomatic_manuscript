36b392f4ce91ecaa8152f5d09d8866dcb422c1cd
Jimin Park <jimin31415@gmail.com>
2025-04-05 01:22:09 -0700

    test

--- deepsomatic_benchmarks/generate_benchmarks_bed.sh
+++ deepsomatic_benchmarks/generate_benchmarks_bed.sh
@@ -36,7 +36,7 @@ echo "other_regions: $other_regions"; fi
 
 if [[ "$name_other_regions" != "" ]] ; then
 echo "name_other_regions: $name_other_regions"; fi
-
+echo "name_other_regions: $name_other_regions"
 
 set -o pipefail
 set -e
@@ -76,7 +76,7 @@ fi
 
 # 3. subtract any other regions
 
-if [[ "$name_other_regions" != "" ]]
+if [[ "$other_regions" != "" && "$name_other_regions" != "" ]]
 then
     if [[ "${CURR_BED}" == ${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSVs_SDs.bed ]]
     then
