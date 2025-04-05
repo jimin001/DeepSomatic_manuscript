4e5c489ae5358a95dd62b5a82fd0c3d8a6b9861c
Jimin Park <jimin31415@gmail.com>
2025-04-05 01:03:45 -0700

    fix spacing

--- deepsomatic_benchmarks/generate_benchmarks_bed.sh
+++ deepsomatic_benchmarks/generate_benchmarks_bed.sh
@@ -104,8 +104,7 @@ then
 elif [[ "${CURR_BED}" == ${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSVs_SDs_${name_other_regions}.bed ]]
 then 
     grep -v "chrX" ${CURR_BED} | grep -v "chrY" > ${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSVs_SDs_${name_other_regions}_chr1_22.bed
-elif
-    [[ "${CURR_BED}" == ${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSDs_${name_other_regions}.bed ]]
+elif [[ "${CURR_BED}" == ${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSDs_${name_other_regions}.bed ]]
 then
     grep -v "chrX" ${CURR_BED} | grep -v "chrY" > ${output_directory}/${date}_${sample}_${output_prefix}_highconf_minusSDs_${name_other_regions}_chr1_22.bed
 fi
